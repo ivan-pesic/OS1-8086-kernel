854f32abf5668fc9113b84a0a24b6a37a6b8f666
ivan-vm <ivan.pesic2000@gmail.com>
2021-08-25 21:53:22 +0200

    Added user thread

--- main.cpp
+++ main.cpp
@@ -7,15 +7,32 @@
 #include "list.h"
 #include "system.h"
 #include "Thread.h"
-#include "Thread.h"
 
 int userMain(int argc, char** argv);
+
+class user_thread : public Thread {
+public:
+	user_thread(int argc, char** argv) : Thread(defaultStackSize, defaultTimeSlice) {
+		this->argc = argc;
+		this->argv = argv;
+	}
+	~user_thread() { waitToComplete(); }
+protected:
+	void run() {
+		userMain(this->argc, this->argv);
+	}
+private:
+	int argc;
+	char** argv;
+};
 
 int main(int argc, char** argv){
 
 	System::inic();
 
-	userMain(argc, argv);
+	user_thread* user = new user_thread(argc, argv);
+	user->start();
+	delete user;
 
 	System::restore();
 
