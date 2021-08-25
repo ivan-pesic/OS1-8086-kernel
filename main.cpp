28a300996d6f0c329b4743f7bccb9cbaf2b63f03
ivan-vm <ivan.pesic2000@gmail.com>
2021-08-26 01:05:48 +0200

    Added some more options to user thread

--- main.cpp
+++ main.cpp
@@ -15,16 +15,29 @@ public:
 	user_thread(int argc, char** argv) : Thread(defaultStackSize, defaultTimeSlice) {
 		this->argc = argc;
 		this->argv = argv;
+		this->ret = 0;
 	}
+
 	~user_thread() { waitToComplete(); }
+
+	virtual Thread* clone() const {
+		return new user_thread(argc, argv);
+	}
+
+	int get_ret() {
+		return ret;
+	}
 protected:
 	void run() {
-		userMain(this->argc, this->argv);
+		ret = userMain(this->argc, this->argv);
 	}
 private:
+	int ret;
 	int argc;
 	char** argv;
 };
+
+int ret_val = 0;
 
 int main(int argc, char** argv){
 
@@ -32,9 +45,11 @@ int main(int argc, char** argv){
 
 	user_thread* user = new user_thread(argc, argv);
 	user->start();
+	user->waitToComplete();
+	ret_val = user->get_ret();
 	delete user;
 
 	System::restore();
 
-	return 0;
+	return ret_val;
 }
